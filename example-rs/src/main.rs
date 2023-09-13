436ca081e2d7de7c83bb9f504d23aa0fde27afed
Ben Collins <benjcollins18@gmail.com>
2023-09-13 14:29:35 +0630

    more exampels

--- example-rs/src/main.rs
+++ example-rs/src/main.rs
@@ -3,7 +3,10 @@ use obbidl_derive::include_obbidl_file;
 include_obbidl_file!("test.txt");
 
 use obbidl::channel::{TestChannel, TestChannelError};
-use test::{cli, ser};
+use test::{
+    cli,
+    ser::{self, S0Receiver},
+};
 
 fn main() {
     thing().unwrap();
